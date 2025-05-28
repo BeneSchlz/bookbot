6c0c0d0834f05a46c9940fd84820d5fd3a80133c
BeneSchlz <166176063+BeneSchlz@users.noreply.github.com>
2025-05-28 11:38:54 +0200

    added books & made a nice report for characters in the text

--- stats.py
+++ stats.py
@@ -5,3 +5,27 @@ def num_words_text(text):
         num_words += 1
     return num_words
  
+def get_num_char(text):
+    text = text.lower()  # Normalize to lowercase
+    char_counts = {}     # Dictionary to hold character counts
+    
+    for char in text:
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+
+    return char_counts
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
